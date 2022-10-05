1afbefff728c10c76789fc3df1f1de481edddacc
noehler <nathanieloehler@gmail.com>
2022-10-04 19:17:54 -0500

    adding classes

--- include/motorControl.h
+++ include/motorControl.h
@@ -1,6 +1,20 @@
 #ifndef __MOTORCONTROL_H__
 #define __MOTORCONTROL_H__
 
+class drivespeeds_t {
+  public:
+    double leftSpd, rightSpd, mechSpd;
+};
+
+
+
+class chassis_t {
+  public:
+    drivespeeds_t driveTrain;
+
+};
+
+
 extern void motorControl(void);
 
 #endif
--- src/motorControl.cpp
+++ src/motorControl.cpp
@@ -1,37 +1,35 @@
 #include "robotConfig.h"
 
-void motorControl(void){
-  while(runLoop){
+double turrControl(void){
     double turrAngle = -float(turretEncoder.get_position())/100/259*12;
     double turrAngleABS  = inertial.get_rotation() + turrAngle;
 
-    static double diffInSpd;
     //std::cout << "\nTAngleRel: " << turrAngle;  
     bool dtb = false;
 
+    static double diffInSpd;
 
     std::cout << "\ninert: " << inertialTurret.get_rotation() << ", enc: " << -turrAngleABS;
 
     if (fabs(-turrAngleABS + inertialTurret.get_rotation()) > 3 && abs(turretEncoder.get_velocity()) < 300){
-      turretEncoder.set_position((-inertialTurret.get_rotation() + inertial.get_rotation())*100*259/12);
-      dtb = 1;
-      std::cout << "\ndiff: " << turrAngleABS + inertialTurret.get_rotation() << ", new: " << -turrAngleABS;
+        turretEncoder.set_position((-inertialTurret.get_rotation() + inertial.get_rotation())*100*259/12);
+        dtb = 1;
+        std::cout << "\ndiff: " << turrAngleABS + inertialTurret.get_rotation() << ", new: " << -turrAngleABS;
     }
     else{
     }
     /*std::cout << "\ndiff: " << fabs(turrAngleABS + inertialTurret.get_rotation()) << ",     IturrAngle: " << inertialTurret.get_rotation() << ",    AbsRot: " << turrAngleABS
-      << ",    IbaseRot: "<< inertial.get_rotation() << ",    vel: " << turretEncoder.get_velocity() << ",     diff2Big: " << dtb;*/
+        << ",    IbaseRot: "<< inertial.get_rotation() << ",    vel: " << turretEncoder.get_velocity() << ",     diff2Big: " << dtb;*/
 
     double turrAngleBet = robotGoal.angleBetweenHorABS + turrAngleABS;
 
     if (turrAngleBet > 180){
-      turrAngleBet -= 360;
+        turrAngleBet -= 360;
     }
     else if( turrAngleBet < -180){
-      turrAngleBet += 360;
+        turrAngleBet += 360;
     }
 
-    int baseSPD;
     //feed forward code todo here, 
     //chassie rotation rate = chassie change of angle in last cycle / elapsed time of last cycle (odom loop)
     //turret target speed = turret ang dif / elapsed time of last cycle (turret twister loop)
@@ -40,18 +38,19 @@ void motorControl(void){
 
     //slowing down turret when nothing is loaded or on deck so intake can run faster
     if (deckLoaded.get_value() > 1900){
-      diffInSpd = pow(fabs(turrAngleBet), 1.4/3)*18; // put that PID here
+        diffInSpd = pow(fabs(turrAngleBet), 1.4/3)*18; // put that PID here
     }
     else{
-      diffInSpd = pow(fabs(turrAngleBet), 1.4/3)*5; // put that PID here
+        diffInSpd = pow(fabs(turrAngleBet), 1.4/3)*5; // put that PID here
     }
-
-    //---------------------this might be the reason turret is turrning the opposite way
     if (turrAngleBet<0){
       diffInSpd *= -1;
     }
- 
+    return diffInSpd;
+}
 
+double intakeControl(double diffInSpd){
+    int baseSPD;
     if (turretValue == 2){
       baseSPD = 100-fabs(diffInSpd);
     }
@@ -61,10 +60,15 @@ void motorControl(void){
     else{
       baseSPD = 0;
     } 
-    //---------------------this might be the reason turret is turrning the opposite way, 
-    //this is not necessary because difference of angle can define the direction of rotation
+    return baseSPD;
+}
 
-    //std::cout << "\nSpeed" << diffInSpd << ", RelA:" << robotGoal.angleBetweenHorREL-(float(turretEncoder.get_position())/100/259*12) << ", bA:" << inertial.get_rotation();
+void motorControl(void){
+  while(runLoop){
+    //getting speeds that diff needs to run at
+    double diffInSpd = turrControl();
+    int baseSPD = intakeControl(diffInSpd);
+
     diff1 = diffInSpd + baseSPD;
     diff2 = -diffInSpd + baseSPD;
     //also put a speed controller for flywheel here, PID is not going to optimal.
